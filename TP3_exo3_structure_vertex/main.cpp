0506bc6376c25b4f2741b35f959305d6cc820fc6
Lucieguerin431 <lucie.guerin@edu.univ-eiffel.fr>
2024-01-24 14:59:10 +0100

    ajout du nv dossier

---
+++ TP3_exo3_structure_vertex/main.cpp
@@ -0,0 +1,93 @@
+#include "p6/p6.h"
+#include "glimac/default_shader.hpp"
+
+
+
+
+int main()
+{
+    auto ctx = p6::Context{{1280, 720, "TP3 EX1"}};
+    ctx.maximize_window();
+
+
+    const p6::Shader shader = p6::load_shader(
+        "shaders/triangle.vs.glsl",
+        "shaders/triangle.fs.glsl"
+    );
+
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    // Bind the VBO
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+
+    GLfloat vertices[] = { 
+        -0.5f, -0.5f, 1.f, 0.f, 0.f, // Premier sommet
+        0.5f, -0.5f, 0.f, 1.f, 0.f, // Deuxième sommet
+        0.0f,  0.5f, 0.f, 0.f, 1.f  // Troisième sommet
+    };
+
+    // Send the vertices to the GPU
+    glBufferData(GL_ARRAY_BUFFER, 15 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+
+    // Unbind the VBO to avoid modification
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+
+    // Bind VAO, 
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    static constexpr GLuint vertex_attr_position = 3;
+    glEnableVertexAttribArray(vertex_attr_position);
+
+    glVertexAttribPointer(vertex_attr_position, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0); 
+
+    static constexpr GLuint vertex_attr_color =8;
+    glEnableVertexAttribArray(vertex_attr_color);
+
+    glVertexAttribPointer(vertex_attr_color, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (const GLvoid*) (3* sizeof(GLfloat)));
+
+    //Unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+    // Unbind the VAO to avoid modification
+    glBindVertexArray(0);
+
+
+
+    ctx.update = [&]() {
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Bind the VAO
+        glBindVertexArray(vao);
+
+     
+        glimac::bind_default_shader();
+
+        // Call shaders
+        shader.use();
+
+        // Draw the triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Unbind the VAO
+        glBindVertexArray(0);
+    };
+
+    ctx.start();
+
+
+    glDeleteBuffers(1, &vbo);
+
+
+    glDeleteVertexArrays(1, &vao);
+}
