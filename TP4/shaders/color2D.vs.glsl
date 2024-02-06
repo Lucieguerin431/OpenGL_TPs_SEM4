2f504a347b6c7c2798ade05903c7444e89cfc077
Lucieguerin431 <lucie.guerin@edu.univ-eiffel.fr>
2024-02-06 09:47:20 +0100

    update color2D.vs (transform)

--- TP4/shaders/color2D.vs.glsl
+++ TP4/shaders/color2D.vs.glsl
@@ -5,7 +5,24 @@ layout(location = 1) in vec3 aVertexColor;
 
 out vec3 vFragColor;
 
+mat3 translate(float tx, float ty) {
+    return mat3(
+        vec3(1, 0, 0),
+        vec3(0, 1, 0),
+        vec3(tx, ty, 1)
+    );
+}
+
+mat3 scale(float sx, float sy) {
+    return mat3(
+        vec3(sx, 0, 0),
+        vec3(0, sy, 0),
+        vec3(0, 0, 1)
+    );
+}
 void main() {
   vFragColor = aVertexColor;
-  gl_Position = vec4(aVertexPosition, 1.5/2, 1.5*2);
+  vec3 transformedPosition = scale(0.5, 0.5) * vec3(aVertexPosition, 1.0);
+  gl_Position = vec4(transformedPosition.xy, 0, 1);
+  
 };
