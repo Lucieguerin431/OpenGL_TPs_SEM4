c411cc361cb254f11fd0c43e5a1cc767df885b10
Lucieguerin431 <lucie.guerin@edu.univ-eiffel.fr>
2024-02-06 10:16:41 +0100

    update color2D.vs (combine transform)

--- TP4/shaders/color2D.vs.glsl
+++ TP4/shaders/color2D.vs.glsl
@@ -20,9 +20,21 @@ mat3 scale(float sx, float sy) {
         vec3(0, 0, 1)
     );
 }
+
+mat3 rotate(float a) {
+    return mat3(
+        vec3(cos(a), sin(a), 0),
+        vec3(-sin(a), cos(a), 0),
+        vec3(0, 0, 1)
+    );
+}
+
 void main() {
   vFragColor = aVertexColor;
-  vec3 transformedPosition = scale(0.5, 0.5) * vec3(aVertexPosition, 1.0);
-  gl_Position = vec4(transformedPosition.xy, 0, 1);
-  
+  vec3 transformedTranslate = translate(0.5,0.5) * vec3(aVertexPosition, 1.0);
+  gl_Position = vec4(transformedTranslate.xy, 0, 1);
+  vec3 transformedRotate = rotate(0.7854) * vec3(transformedTranslate.xy, 1.0);
+  gl_Position = vec4(transformedRotate.xy, 0, 1);
+  vec3 transformedScale = scale(2,2) * vec3(transformedRotate.xy, 1.0);
+  gl_Position = vec4(transformedScale.xy, 0, 1);
 };
